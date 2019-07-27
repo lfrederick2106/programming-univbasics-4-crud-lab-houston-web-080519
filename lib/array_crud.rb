9fe28118aa5fafefef8cc371ac1aac98cef60d3f
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-27 20:34:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [fruit, vegetable, grain, fat
+  fruits = [banana, apple, watermelon, pear]
 end
 
 def add_element_to_end_of_array(array, element)
